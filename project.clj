2b69b9e7d2c55d0e7a385297331a566f8cb380c2
tormaroe <torbjorn.maro@gmail.com>
2011-05-23 22:43:35 +0200

    added lein-clojar dep

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
-(defproject marosoft-clj "1.0.0-SNAPSHOT"
-  :description "FIXME: write"
+(defproject marosoft-clj "0.0.1"
+  :description "A jar of stuff"
   :dependencies [[org.clojure/clojure "1.2.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]])
+                 [org.clojure/clojure-contrib "1.2.0"]]
+  :dev-dependencies [[lein-clojars "0.6.0"]])
